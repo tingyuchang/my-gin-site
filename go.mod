d64a6ae92ebb7109e62675415759b6fdf5d60cfc
tingyuchang <tingyugenius@gmail.com>
2020-05-07 11:44:59 +0800

    using go module

---
+++ go.mod
@@ -0,0 +1,5 @@
+module 33tech/demo-site
+
+go 1.13
+
+require github.com/gin-gonic/gin v1.6.3
--- main.go
+++ main.go
@@ -5,10 +5,11 @@ import (
 )
 
 func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Success",
+			"msg": "Success",
 		})
 	})
 
